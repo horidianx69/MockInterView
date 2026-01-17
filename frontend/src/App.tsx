3e753797ff4fee4e6dfc8184b5904da32f0b4717
horidianx69 <ayush.shuklaps@gmail.com>
2026-01-17 21:07:18 +0530

    first commit project setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-300">Hello world!</h1>
+      <Button>Click Me</Button>
+    </>
+  );
+}
+
+export default App;
