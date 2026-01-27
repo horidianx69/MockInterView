7496a5ab1c8bd0525ff9db933f78dfa3b3f80de5
horidianx69 <ayush.shuklaps@gmail.com>
2026-01-27 20:23:43 +0530

    toggle bana diya hai shadcn se

--- frontend/src/components/toggle-container.tsx
+++ frontend/src/components/toggle-container.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import { Menu } from "lucide-react";
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
+import NavigationRoutes from "./navigation-routes";
+import { cn } from "@/lib/utils";
+import { useAuth } from "@clerk/clerk-react"
+import { NavLink } from "react-router-dom";
+
 
 const ToggleContainer = () => {
+  const { userId } = useAuth();
   return (
-    <div>
-      ToggleContainer
-    </div>
+    <Sheet>
+      <SheetTrigger><Menu className="block md:hidden"/></SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Mockery💕</SheetTitle>
+          <SheetDescription>
+            Prepping made easy, anywhere you go.
+          </SheetDescription>
+        </SheetHeader>
+         <NavigationRoutes isMobile={true} />
+          {userId && <NavLink to="/dashboard" className={({isActive})=>cn("text-base text-neutral-600",isActive && "text-neutral-900 font-semibold")}>Begin Interview</NavLink>}
+      </SheetContent>
+    </Sheet>
   );
 }
 
 export default ToggleContainer;
+  
